658ccd913e500dbcd1aa6f6572cd381f86b9f127
Maria Barros <48946383+44Mozart@users.noreply.github.com>
2019-12-09 18:27:25 +0000

    Update hashing.c

--- Tabelas de Hash/hashing.c
+++ Tabelas de Hash/hashing.c
@@ -39,28 +39,82 @@ void initHT(HT *h, int size) {
 
 
 int freeHT(HT *h, int k) {
-    
-    return freeHT_sol(h, k);
+    // Ver se está livre, ou ocupado ou apagado
+    if (h->tbl[k].key == EMPTY) return 1;
+    if (h->tbl[k].key == DELETED) return 1;
+    return 0;
 }
 
+void growHT (HT *h, int size) {
+    int i, j;
+    struct pair *aux = calloc(size, (sizeof(struct pair)));
+    
+    // Porquê o strcpy? Não podias por manualmente tipo:
+    // for (i = 0; i < size; i++) aux->tbl[i].key == EMPTY
+    for (i = 0; i < size; i++) strcpy (aux[i].key, EMPTY);
+
+    for (i = 0; i < h->size; i++) {
+        if (!freeHT(h,i)){
+            j = hash(h->tbl[i].key, size);
+            while (strcmp(aux[j].key,EMPTY) != 0) {
+                // Enquanto não encontra um livre
+                j = (j+1)%2;
+            }
+            strcpy(aux[j].key,h->tbl[i].key);
+            aux[j].value = h->tbl[i].value;
+        }
+    }
+    free(h->tbl);
+    h->tbl = aux;
+    h->size = size;
+}
 
 
 int writeHT (HT *h, char key[], int value) {
-
-    return writeHT_sol (h, key, value);
+    //adicionar um elemento há tabela com valor value e chave key
+    int i;
+    if (h->used > 0.8*h->size) growHT(h,2 * h->size);
+    i = hash(key,h->size);
+    while (!freeHT(h,i)){
+        i = (i+1)%(h->size);
+    }
+    h->used ++;
+    h->tbl[i].value = value;
+    strcpy(h->tbl[i].key,key);
+    return i;
 }
 
-
-
 int readHT (HT *h, char key[], int* value) {
+    int i;
+    i = hash(key,h->size);
+    while (!freeHT(h,i)){
+        if (strcpy(h->tbl[i].key,key) == 0){
+            *value = h->tbl[i].value;
+            return i;
+        }
+        i = (i+1)%(h->size);
+    }
     
-    return readHT_sol (h, key, value);
+    
+    return -1;
 }
 
 
 
 int deleteHT (HT *h, char key[]) {
-
-    return deleteHT_sol (h, key);
+    int i = hash(key,h->size);
+    
+    while (!freeHT(h,i)) {
+        if (strcmp(h->tbl[i].key, key) == 0){
+            strcpy(h->tbl[i].key, DELETED);
+            h->used --;
+            return i;
+        }
+        i = (i+1)%(h->size);
+    }
+    return -1;
 }
+
+
+
 
